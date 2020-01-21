54427c00f292caea73afe182ae9d657d1d4d0f18
Lukas Taylor <ltaylor@luktay-localdomain.internal>
2020-01-20 21:04:14 -0800

    changes to deck logic;successfully created deck

--- card.py
+++ card.py
@@ -1,57 +1,36 @@
-#import random module
-import random
-
-class Card:
-    
-    def __init__(self):
-        super().__init__()
-        
-    def getSuit(self):
-        suitLst = ['S', 'C', 'D', 'H']
-        #return random index in lst
-        return suitLst[random.randint(0, 3)]
-        
-    def getRank(self):
-        rankLst = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-        #return random index in lst
-        return rankLst[random.randint(0,8)]
-    
-    def getFace(self):
-        faceLst = ['J', 'Q', 'K', 'A']
-        #return random index in lst
-        return faceLst[random.randint(0, 3)]
-
-    def createCard(self):
-        suit = self.getSuit()
-        rank = self.getRank()
-        face = self.getFace()
-        #return tuple which represents single card
-        return (rank, face, suit)
-
 class Deck:
     def __init__(self):
         super().__init__()
+        self.cardLst = []
+        self.cardPair = ()
     
-    def createDeck(self):
-        deckSize = 0
-        deckLst = []
-        while(deckSize < 52):
-            cardTuple = card.createCard()
-            deckLst.append(cardTuple)
-            deckSize += 1
-        return deckLst
-    
+    def getCard(self):
+        suitLst = ['S', 'C', 'D', 'H']
+        rankLst = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+        faceLst = ['J', 'Q', 'K', 'A']
+        i = 0
+        j = 0
+        #pair ranks with suits
+        for i in range(len(rankLst)):
+            for j in range(len(suitLst)):
+                self.cardPair = (rankLst[i], suitLst[j])
+                self.cardLst.append(self.cardPair)
+                j+=1
+            i +=1
+        #pair faces with suits
+        for i in range(len(faceLst)):
+            for j in range(len(suitLst)):
+                self.cardPair = (faceLst[i], suitLst[j])
+                self.cardLst.append(self.cardPair)
+                j+=1
+            i +=1
+
     def printDeck(self):
-        deckLst = self.createDeck()
         i = 0
         for i in range(52):
             print(i)
-            print(deckLst[i])
+            print(self.cardLst[i])
 
-card = Card()
 deck = Deck()
+deck.getCard()
 deck.printDeck()
-         
-
-#two.printCard()
-#cardTuple = card.createCard()
